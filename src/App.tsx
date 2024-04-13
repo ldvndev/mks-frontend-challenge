c8f45b1e3ab218c275d17a936284ffdbd68154e4
ldvndev <ldvndev@gmail.com>
2024-04-13 15:09:14 -0300

    feat: Application structure - Configuring themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <div />
+  return <ThemeProvider theme={defaultTheme}></ThemeProvider>
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,10 @@
+export const defaultTheme = {
+  brand: '#0F52BA',
+  white: 'white',
+  black: 'black',
+
+  'gray-100': '#F9F9F9',
+  'gray-300': '#EEEEEE',
+  'gray-600': '#2C2C2C',
+  'gray-700': '#373737',
+} as const
